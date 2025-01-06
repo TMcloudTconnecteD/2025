ec77d35f80a3f871aa420f632ddc5f70b705c89f
AoB 23 <larrygerald76@gmail.com>
2025-01-06 19:49:01 +0000

    commit2.o

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,43 +1,43 @@
 // File: src/data/data.tsx
 export const categories = [
-  { id: 1, name: "Liquor", image: "/images/liquor.jpg" },
-  { id: 2, name: "Shopping", image: "/images/shopping.jpg" },
-  { id: 3, name: "Fashion", image: "/images/fashion.jpg" },
-  { id: 4, name: "Groceries", image: "/images/shopping.jpg" },
-  { id: 5, name: "RideBoda", image: "/images/fashion.jpg" },
+  { id: 1, name: "Liquor", image: "/images/liquor0.jpg" },
+  { id: 2, name: "Shopping", image: "/images/fashion0.jpg" },
+  { id: 3, name: "Fashion", image: "/images/fashion3.jpg" },
+  { id: 4, name: "Groceries", image: "/images/groceries.jpg" },
+  { id: 5, name: "RideBoda", image: "/images/rent_bike.jpg" },
   // Add more categories if needed
 ];
 
 export const shops = {
   1: [
-    { name: "Honey Pot", image: "/images/honey-pot.jpg" },
-    { name: "Mr Beast Yaya", image: "/images/mr-beast.jpg" },
-    { name: "Jupyter Wines", image: "/images/jupyter-wines.jpg" },
-    { name: "ToshBantu", image: "/images/jupyter-wines.jpg" },
+    { name: "Honey Pot", image: "/images/product1.jpg" },
+    { name: "Mr Beast Yaya", image: "/images/liquor2.jpg" },
+    { name: "Jupyter Wines", image: "/images/liquor0.jpg" },
+    { name: "ToshBantu", image: "/images/liquor2.jpg" },
   ],
   2: [
-    { name: "Rose Shop", image: "/images/rose-shop.jpg" },
-    { name: "Ali Ownio Shop", image: "/images/al-owno.jpg" },
-    { name: "Easthter Shop", image: "/images/easthter.jpg" },
-    { name: "Comrades Shop", image: "/images/al-owno.jpg" },
-    { name: "Famous Shop", image: "/images/easthter.jpg" },
+    { name: "Rose Shop", image: "/images/groceries.jpg" },
+    { name: "Ali Ownio Shop", image: "/images/groceries.jpg" },
+    { name: "Easthter Shop", image: "/images/groceries.jpg" },
+    { name: "Comrades Shop", image: "/images/groceries.jpg" },
+    { name: "Famous Shop", image: "/images/groceries.jpg" },
   ],
   3: [
-    { name: "Zara", image: "/images/zara.jpg" },
-    { name: "H&M", image: "/images/h&m.jpg" },
-    { name: "Uniqlo", image: "/images/uniqlo.jpg" },
+    { name: "Zara", image: "/images/fashion0.jpg" },
+    { name: "H&M", image: "/images/fashion2.jpg" },
+    { name: "Uniqlo", image: "/images/fashion3.jpg" },
   ],
 
   4: [
-    { name: "soko1", image: "/images/zara.jpg" },
-    { name: "Gakoromone", image: "/images/h&m.jpg" },
-    { name: "Texas", image: "/images/uniqlo.jpg" },
+    { name: "soko1", image: "/images/fashion3.jpg" },
+    { name: "Gakoromone", image: "/images/fashion2.jpg" },
+    { name: "Texas", image: "/images/fashion1.jpg" },
   ],
 
   5: [
-    { name: "Anto", image: "/images/zara.jpg" },
-    { name: "Muchui", image: "/images/h&m.jpg" },
-    { name: "Kevo", image: "/images/uniqlo.jpg" },
+    { name: "Anto", image: "/images/rent_bike.jpg" },
+    { name: "Muchui", image: "/images/rent_bike.jpg" },
+    { name: "Kevo", image: "/images/rent_bike.jpg" },
   ],
 };
 // File: src/data/data.ts
